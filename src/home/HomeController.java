da86ad97ea243f5a3cb1446fef277d23c51aa854
ckan2010@gmail.com <ckan2010@gmail.com>
2016-07-20 12:33:59 +0900

    2016-07-20 mvc test

---
+++ src/home/HomeController.java
@@ -0,0 +1,38 @@
+package home;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javafx.scene.shape.Path;
+import jdk.nashorn.internal.ir.RuntimeNode.Request;
+
+/**
+ * Servlet implementation class HomeController
+ */
+@WebServlet({"/home.do","/top.do","/header.do"})
+public class HomeController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String servletPath = request.getServletPath();
+		String path = servletPath.split("/")[1];
+		String view = path.substring(0, path.indexOf("."));
+		RequestDispatcher dis = request.getRequestDispatcher("/WEB-INF/global/"+view+".jsp");
+		dis.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
